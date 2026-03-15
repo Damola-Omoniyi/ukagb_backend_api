2afb2215828de15e21beb24730a4500b8b366f5a
Adedamola <adedamolaomoniyi99@gmail.com>
2026-03-15 14:39:03 +0000

    Added indexes and unique constraints to the models and ran migrations

--- uk_alevel_grade_boundaries/exam_data/models.py
+++ uk_alevel_grade_boundaries/exam_data/models.py
@@ -3,25 +3,40 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.timezone import now
 
 class ExamBoard(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 class Subject(models.Model):
     exam_board = models.ForeignKey(ExamBoard, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, db_index=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['exam_board'])]
+
+    def __str__(self):
+        return f"{self.exam_board} {self.name}"
 
 class OverallGradeBoundary(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    exam_board = models.ForeignKey(ExamBoard, on_delete=models.CASCADE)
     year = models.IntegerField(
         validators=[
             MinValueValidator(2015),
             MaxValueValidator(now().year)
-        ]
-    )
+        ])
     grade = models.CharField(max_length=10)
     grade_threshold = models.IntegerField(validators=[ MinValueValidator(0)])
+    
+    class Meta:
+        indexes = [models.Index(fields=['subject','year'])]
+        constraints = [models.UniqueConstraint(fields=['subject', 'year', 'grade'], name='unique_boundary')]
+
+    def __str__(self):
+        return f"{self.subject}  {self.year} {self.grade} : {self.grade_threshold}"
 
 class GradeStatistic(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     year = models.IntegerField(
         validators=[
             MinValueValidator(2015),
@@ -36,11 +51,24 @@ class GradeStatistic(models.Model):
     )
     num_students = models.IntegerField(validators=[ MinValueValidator(0)])
 
+    class Meta:
+        indexes =  [models.Index(fields=['subject', 'year'])]
+
+    def __str__(self):
+        return f"{self.subject}  {self.year} {self.grade} : {self.percentage}"
+
 class ComponentGradeBoundary(models.Model):
     paper_number = models.CharField(max_length=10)
     overall_grade_boundary = models.ForeignKey(OverallGradeBoundary, on_delete= models.CASCADE)
     grade = models.CharField(max_length=10)
     grade_threshold = models.IntegerField(validators=[ MinValueValidator(0)])
+
+    class Meta:
+        indexes = [models.Index(fields=['overall_grade_boundary','paper_number'])]
+        constraints = [models.UniqueConstraint(fields=['overall_grade_boundary','paper_number', 'grade'], name='unique_component_boundary')]
+
+    def __str__(self):
+        return f"{self.overall_grade_boundary.subject} {self.overall_grade_boundary.year} paper {self.paper_number} {self.grade} : {self.grade_threshold}"
 
 class DocumentURL(models.Model):
     DOCUMENT_TYPES = [("past_paper", "Past Paper"),
@@ -49,3 +77,9 @@ class DocumentURL(models.Model):
     paper = models.ForeignKey(ComponentGradeBoundary, on_delete=models.CASCADE)
     document_type = models.CharField(max_length=20, choices = DOCUMENT_TYPES)
     url = models.URLField(max_length=500)
+
+    class Meta:
+        indexes = [models.Index(fields=['paper', 'document_type'])]
+
+    def __str__(self):
+        return f"{self.paper} {self.document_type}"
