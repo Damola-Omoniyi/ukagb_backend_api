f1f485605519ab2dbd9255f9885234b4eb9efbc7
Adedamola <adedamolaomoniyi99@gmail.com>
2026-04-09 21:32:15 +0100

    I have written the API views and set up the appropraite urls

---
+++ uk_alevel_grade_boundaries/exam_data/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from exam_data import views
+
+urlpatterns = [
+    path("exam-boards/", views.ExamBoardList.as_view(), name="exam-board-list"),
+    path("<str:exam_board_name>/subjects/", views.SubjectList.as_view(), name="subject-list"),
+    path("<str:exam_board_name>/<str:subject_name>/GradeBoundaries/", views.GradeBoundaryList.as_view(), name="grade-boundaries-list"),
+    path("<str:exam_board_name>/<str:subject_name>/statistics/", views.GradeStatisticList.as_view(), name = "grade-statistic-list"),
+    path("<str:exam_board_name>/<str:subject_name>/<int:year>/", views.ComponentGradeList.as_view(), name="component-grade-list"),
+    path("<str:exam_board_name>/<str:subject_name>/<int:year>/<int:paper_number>/", views.DocumentURLList.as_view(), name="url-list")
+]
+
