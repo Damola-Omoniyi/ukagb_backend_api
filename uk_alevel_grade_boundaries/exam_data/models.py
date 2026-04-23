c22fe2b5201c3e4bbee86dcf5bc0bfe677bdd2fb
Adedamola <adedamolaomoniyi99@gmail.com>
2026-04-23 19:15:23 +0100

    Updated the database and written tests for the new db design

--- uk_alevel_grade_boundaries/exam_data/models.py
+++ uk_alevel_grade_boundaries/exam_data/models.py
@@ -3,46 +3,51 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.timezone import now
 
 class ExamBoard(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, unique=True)
 
     def __str__(self):
         return self.name
 
 class Subject(models.Model):
     exam_board = models.ForeignKey(ExamBoard, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200)
 
     class Meta:
-        indexes = [models.Index(fields=['exam_board'])]
+        indexes = [models.Index(fields=['exam_board', 'name'])]
+        constraints = [models.UniqueConstraint(fields=['exam_board', 'name'], name='unique_subject')]
 
     def __str__(self):
-        return f"{self.exam_board} {self.name}"
-
-class OverallGradeBoundary(models.Model):
+        return f"{self.exam_board.name} {self.name}"
+    
+class ExamSession(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     year = models.IntegerField(
         validators=[
             MinValueValidator(2015),
             MaxValueValidator(now().year)
         ])
-    grade = models.CharField(max_length=10)
-    grade_threshold = models.IntegerField(validators=[ MinValueValidator(0)])
     
     class Meta:
-        indexes = [models.Index(fields=['subject','year'])]
-        constraints = [models.UniqueConstraint(fields=['subject', 'year', 'grade'], name='unique_boundary')]
+        indexes = [models.Index(fields=['subject', 'year'])]
+        constraints = [models.UniqueConstraint(fields=['subject', 'year'], name='unique_exam')]
+    
+    def __str__(self):
+        return f"{self.subject} {self.year}"
+
+class OverallGradeBoundary(models.Model):
+    exam_session = models.ForeignKey(ExamSession, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=10)
+    grade_threshold = models.IntegerField(validators=[ MinValueValidator(0)])
+    class Meta:
+        indexes = [models.Index(fields=['exam_session'])]
+        constraints = [models.UniqueConstraint(fields=['exam_session', 'grade'], name='unique_boundary')]
 
     def __str__(self):
-        return f"{self.subject}  {self.year} {self.grade} : {self.grade_threshold}"
+        return f"{self.exam_session} {self.grade} : {self.grade_threshold}"
+
 
 class GradeStatistic(models.Model):
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    year = models.IntegerField(
-        validators=[
-            MinValueValidator(2015),
-            MaxValueValidator(now().year)
-        ]
-    )
+    exam_session = models.ForeignKey(ExamSession, on_delete=models.CASCADE)
     grade = models.CharField(max_length=10)
     percentage = models.DecimalField(
         max_digits=5,  
@@ -52,36 +57,46 @@ class GradeStatistic(models.Model):
     num_students = models.IntegerField(validators=[ MinValueValidator(0)])
 
     class Meta:
-        indexes =  [models.Index(fields=['subject', 'year'])]
-        constraints = [models.UniqueConstraint(fields=['subject', 'year', 'grade'], name='unique_statistic')]
-
+        indexes =  [models.Index(fields=['exam_session'])]
+        constraints = [models.UniqueConstraint(fields=['exam_session', 'grade'], name='unique_statistic')]
 
     def __str__(self):
-        return f"{self.subject}  {self.year} {self.grade} : {self.percentage}"
+        return f"{self.exam_session} {self.grade} : {self.percentage}"
+    
+class ExamPaper(models.Model):
+    exam_session = models.ForeignKey(ExamSession, on_delete=models.CASCADE)
+    paper_number = models.CharField(max_length=10)
+
+    class Meta:
+        indexes =[models.Index(fields=['exam_session'])]
+        constraints = [models.UniqueConstraint(fields=['exam_session','paper_number'], name = 'unique_paper')]
+    
+    def __str__(self):
+        return f"{self.exam_session} paper:{self.paper_number}"
 
 class ComponentGradeBoundary(models.Model):
-    paper_number = models.CharField(max_length=10)
-    overall_grade_boundary = models.ForeignKey(OverallGradeBoundary, on_delete= models.CASCADE)
+    exam_paper = models.ForeignKey(ExamPaper, on_delete=models.CASCADE)
     grade = models.CharField(max_length=10)
     grade_threshold = models.IntegerField(validators=[ MinValueValidator(0)])
 
     class Meta:
-        indexes = [models.Index(fields=['overall_grade_boundary','paper_number'])]
-        constraints = [models.UniqueConstraint(fields=['overall_grade_boundary','paper_number', 'grade'], name='unique_component_boundary')]
+        indexes = [models.Index(fields=['exam_paper'])]
+        constraints = [models.UniqueConstraint(fields=['exam_paper', 'grade'], name='unique_component_boundary')]
 
     def __str__(self):
-        return f"{self.overall_grade_boundary.subject} {self.overall_grade_boundary.year} paper {self.paper_number} {self.grade} : {self.grade_threshold}"
+        return f"{self.exam_paper} {self.grade} - {self.grade_threshold}"
 
 class DocumentURL(models.Model):
     DOCUMENT_TYPES = [("past_paper", "Past Paper"),
                       ("mark_scheme", "Mark Scheme"),
                       ("exam_report", "Exam Report")]
-    paper = models.ForeignKey(ComponentGradeBoundary, on_delete=models.CASCADE)
+    paper = models.ForeignKey(ExamPaper, on_delete=models.CASCADE)
     document_type = models.CharField(max_length=20, choices = DOCUMENT_TYPES)
     url = models.URLField(max_length=500)
 
     class Meta:
         indexes = [models.Index(fields=['paper', 'document_type'])]
+        constraints = [models.UniqueConstraint(fields=['paper','document_type'], name='unique_doc')]
 
     def __str__(self):
         return f"{self.paper} {self.document_type}"
---
+++ uk_alevel_grade_boundaries/exam_data/tests/test_models.py
@@ -0,0 +1,102 @@
+from django.test import TestCase
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+
+from exam_data.models import (ExamBoard, Subject, ExamSession, OverallGradeBoundary, GradeStatistic, ExamPaper, 
+                              ComponentGradeBoundary, DocumentURL)
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.exam_board = ExamBoard.objects.create(name = "AQA")
+        self.subject = Subject.objects.create(exam_board = self.exam_board, name = "Physics")
+        self.exam_session = ExamSession.objects.create(subject=self.subject, year=2021)
+        self.boundary = OverallGradeBoundary.objects.create(exam_session=self.exam_session, grade='A', grade_threshold=100)
+        self.statistic = GradeStatistic.objects.create(exam_session=self.exam_session, grade="A", percentage=12.5, num_students = 40)
+        self.exam_paper = ExamPaper.objects.create(exam_session = self.exam_session, paper_number="1")
+        self.component_boundary = ComponentGradeBoundary.objects.create(exam_paper=self.exam_paper, grade="A", grade_threshold=60)
+        self.url = 'https://www.aqa.org.uk/files/sample-papers-and-mark-schemes.2022.june.AQA-71921-QP-JUN22_PDF/5ada22f946e7ffd420d45c1cef788840c42f261c.pdf'
+        self.document = DocumentURL.objects.create(paper=self.exam_paper, document_type="past_paper", url=self.url)
+
+    # Tests for Subject Model
+    def test_duplicate_subject(self):
+        with self.assertRaises(IntegrityError):
+            Subject.objects.create(exam_board=self.exam_board, name="Physics")
+        
+    # Tests for the ExamSession Model
+    def test_exam_session_year_too_low(self):
+        exam_session = ExamSession(subject = self.subject, year = 1990)
+        with self.assertRaises(ValidationError):
+            exam_session.full_clean()
+        
+    def test_exam_session_year_too_high(self):
+        exam_session = ExamSession(subject=self.subject, year=now().year+10)
+        with self.assertRaises(ValidationError):
+            exam_session.full_clean()
+    
+    def test_duplicate_exam_session(self):
+        with self.assertRaises(IntegrityError):
+            ExamSession.objects.create(subject=self.subject, year=2021)
+        
+    # Tests for OverallBoundary Model
+    def test_grade_threshold_too_low(self):
+        boundary = OverallGradeBoundary(exam_session=self.exam_session, grade='C', grade_threshold=-20)
+        with self.assertRaises(ValidationError):
+            boundary.full_clean()
+
+    def test_duplicate_boundary(self):
+        with self.assertRaises(IntegrityError):
+            OverallGradeBoundary.objects.create(exam_session=self.exam_session, grade='A', grade_threshold=150)
+    
+    # Test for GradeStatistic Model
+    def test_percentage_too_low(self):
+        statistic = GradeStatistic(exam_session=self.exam_session, grade="C", percentage=-10, num_students = 40)
+        with self.assertRaises(ValidationError):
+            statistic.full_clean()
+    
+    def test_percentage_too_high(self):
+        statistic = GradeStatistic(exam_session=self.exam_session, grade="D", percentage=101, num_students = 40)
+        with self.assertRaises(ValidationError):
+            statistic.full_clean()
+    
+    def test_num_students_too_low(self):
+        statistic = GradeStatistic(exam_session=self.exam_session, grade="B", percentage=10, num_students = -40)
+        with self.assertRaises(ValidationError):
+            statistic.full_clean()
+    
+    def test_duplicate_stat(self):
+        with self.assertRaises(IntegrityError):
+            GradeStatistic.objects.create(exam_session=self.exam_session, grade="A", percentage=17.5, num_students = 60)
+        
+    # Test for Exam Paper - test for duplicates
+    def test_duplicate_exam_paper(self):
+        with self.assertRaises(IntegrityError):
+            ExamPaper.objects.create(exam_session = self.exam_session, paper_number="1")
+
+    # Test for component boundary - test grade threshold and duplicates
+    def test_paper_threshold_too_low(self):
+        component_boundary = ComponentGradeBoundary(exam_paper=self.exam_paper, grade="B", grade_threshold=-30)
+        with self.assertRaises(ValidationError):
+            component_boundary.full_clean()
+    
+    def test_duplicate_paper(self):
+        with self.assertRaises(IntegrityError):
+            ComponentGradeBoundary.objects.create(exam_paper=self.exam_paper, grade="A", grade_threshold=80)
+
+    # Test URLS - test duplicates
+    def test_duplicate_documents(self):
+        with self.assertRaises(IntegrityError):
+            DocumentURL.objects.create(paper=self.exam_paper, document_type="past_paper", url="https://chatgpt.com/?temporary-chat=true")
+
+            
+
+        
+        
+    
+    
+
+
+
+        
+    
+        
