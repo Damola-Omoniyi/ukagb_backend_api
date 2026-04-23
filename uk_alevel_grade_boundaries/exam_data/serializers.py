f257ded4e7429a4da9fa7cd2d81e5c98e10732f8
Adedamola <adedamolaomoniyi99@gmail.com>
2026-04-23 19:41:50 +0100

    I have updated the serializers with minor changes to reflect the db design changes

--- uk_alevel_grade_boundaries/exam_data/serializers.py
+++ uk_alevel_grade_boundaries/exam_data/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from exam_data.models import (ExamBoard, Subject, OverallGradeBoundary,
-                              GradeStatistic, ComponentGradeBoundary, DocumentURL)
+from exam_data.models import (ExamBoard, Subject, ExamSession, OverallGradeBoundary, GradeStatistic, ExamPaper, 
+                              ComponentGradeBoundary, DocumentURL)
 
 class ExamBoardSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,20 +12,30 @@ class SubjectSerializer(serializers.ModelSerializer):
         model = Subject
         fields = ["id", "name"]
 
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamSession
+        fields = ["id","year"]
+
 class OverallGradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = OverallGradeBoundary
-        fields = ["id", "year", "grade", "grade_threshold"]
+        fields = ["id", "grade", "grade_threshold"]
 
 class ComponentGradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ComponentGradeBoundary
-        fields = ["id", "paper_number", "grade", "grade_threshold"]
+        fields = ["id", "grade", "grade_threshold"]
 
 class GradeStatisticSerializer(serializers.ModelSerializer):
     class Meta:
         model = GradeStatistic
         fields = ["id", "grade", "percentage", "num_students"]
+
+class PaperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamPaper
+        fields = ["id", "paper_number"]
 
 class URLSerializer(serializers.ModelSerializer):
     class Meta:
