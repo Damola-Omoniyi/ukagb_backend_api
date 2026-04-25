63533047b85e7e5ba626bf9393b1fed61c9e1213
Adedamola <adedamolaomoniyi99@gmail.com>
2026-04-25 17:00:43 +0100

    registered models with admin

--- uk_alevel_grade_boundaries/exam_data/admin.py
+++ uk_alevel_grade_boundaries/exam_data/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from exam_data.models import (ExamBoard, Subject, ExamSession, OverallGradeBoundary, GradeStatistic, ExamPaper, 
+                              ComponentGradeBoundary, DocumentURL)
 
-# Register your models here.
+admin.site.register(ExamBoard)
+admin.site.register(Subject)
+admin.site.register(ExamSession)
+admin.site.register(OverallGradeBoundary)
+admin.site.register(GradeStatistic)
+admin.site.register(ExamPaper)
+admin.site.register(ComponentGradeBoundary)
+admin.site.register(DocumentURL)
